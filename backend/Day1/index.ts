f3cad060c1be60e8328b80a10e2350fa55912615
Abhishekweb3 <abhishek@web3task.com>
2026-02-02 16:05:17 +0530

    Explicit type with all types of variables doen (including array)

--- backend/Day1/index.ts
+++ backend/Day1/index.ts
@@ -1,1 +1,5 @@
-let fullName:string = "Abhishek Gupta"
+let fullName:string = "Abhishek Gupta";
+let phoneNumber:number = 64654644
+let isStudent:boolean = true;
+let skills:string[] = ["HTML","CSS","Javascript","tailwind"]
+let marks: number[] = [90,89,23,43];
